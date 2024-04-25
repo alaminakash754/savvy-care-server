4698b342a142156cb5900fa4a6963d80c5e13cf0
alamin <alaminakash9754@gmail.com>
2024-04-25 14:09:09 +0600

    added payment collection to the data base

--- index.js
+++ index.js
@@ -1,160 +1,190 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config()
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
-// middleware 
+// middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i6c2rzu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
-        const doctorCollection = client.db('savvyCareDb').collection("doctors");
-        const userCollection = client.db("savvyCareDb").collection("users");
-        const treatmentCollection = client.db("savvyCareDb").collection("treatments");
-        const appointmentCollection = client.db("savvyCareDb").collection("appointments");
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const doctorCollection = client.db("savvyCareDb").collection("doctors");
+    const userCollection = client.db("savvyCareDb").collection("users");
+    const treatmentCollection = client
+      .db("savvyCareDb")
+      .collection("treatments");
+    const appointmentCollection = client
+      .db("savvyCareDb")
+      .collection("appointments");
+    const paymentCollection = client.db("savvyCareDb").collection("payments");
 
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
-
-        // jwt related api
-        app.post('/jwt', async (req, res) => {
-            const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn: '1h'
-            });
-            res.send({ token });
-        })
-
-        // middlewares verify token 
-        const verifyToken = (req, res, next) => {
-            // console.log('inside verify token', req.headers.authorization);
-            if (!req.headers.authorization) {
-                return res.status(401).send({ message: 'unauthorized access' });
-            }
-            const token = req.headers.authorization.split(' ')[1];
-            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-                if (err) {
-                    return res.status(401).send({ message: 'unauthorized access' })
-                }
-                req.decoded = decoded;
-                next();
-            })
+    // middlewares verify token
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
         }
-        // user related api 
+        req.decoded = decoded;
+        next();
+      });
+    };
+    // user related api
 
-        app.get('/users', async (req, res) => {
-            console.log(req.headers);
-            const result = await userCollection.find().toArray();
-            res.send(result);
-        })
+    app.get("/users", async (req, res) => {
+      console.log(req.headers);
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-            const query = { email: user.email };
-            const existingUser = await userCollection.findOne(query);
-            if (existingUser) {
-                return res.send({ message: 'user already exists', insertedId: null })
-            }
-            const result = await userCollection.insertOne(user);
-            res.send(result);
-        })
+    // admin related api
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
-        // admin related api 
-        app.patch('/users/admin/:id',  async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) }
-            const updatedDoc = {
-                $set: {
-                    role: 'admin'
-                }
-            }
-            const result = await userCollection.updateOne(filter, updatedDoc);
-            res.send(result);
-        })
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
 
-        app.get('/users/admin/:email', verifyToken, async (req, res) => {
-            const email = req.params.email;
-            if (email !== req.decoded.email) {
-                return res.status(403).send({ message: 'forbidden access' })
-            }
-            const query = { email: email };
-            const user = await userCollection.findOne(query);
-            let admin = false;
-            if (user) {
-                admin = user?.role === 'admin';
-            }
-            res.send({ admin });
-        })
+    // doctor related api
+    app.post("/doctors", async (req, res) => {
+      const doctor = req.body;
+      const result = await doctorCollection.insertOne(doctor);
+      res.send(result);
+    });
 
-// doctor related api 
-        app.post('/doctors', async (req, res) => {
-            const doctor = req.body;
-            const result = await doctorCollection.insertOne(doctor);
-            res.send(result);
-        })
+    app.get("/doctors", async (req, res) => {
+      const cursor = doctorCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    // treatment api
+    app.get("/treatments", async (req, res) => {
+      const result = await treatmentCollection.find().toArray();
+      res.send(result);
+    });
 
-        app.get('/doctors', async (req, res) => {
-            const cursor = doctorCollection.find()
-            const result = await cursor.toArray();
-            res.send(result);
-        })
+    // appointments related api
+    app.post("/appointments", async (req, res) => {
+      const appointment = req.body;
+      console.log(appointment);
+      const result = await appointmentCollection.insertOne(appointment);
+      result.appointment = appointment;
+      res.send(result);
+    });
 
-        // treatment api 
-        app.get('/treatments', async (req, res) => {
-            const result = await treatmentCollection.find().toArray();
-            res.send(result);
-        })
+    app.get("/appointments", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await appointmentCollection.find(query).toArray();
+      res.send(result);
+    });
 
-        // appointments related api 
-        app.post('/appointments', async (req, res) => {
-            const appointment = req.body;
-            console.log(appointment)
-            const result = await appointmentCollection.insertOne(appointment);
-            result.appointment=appointment;
-            res.send(result);
-        })
+    // payment related api
+    // payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
 
-        app.get('/appointments', async(req, res) => {
-            const email = req.query.email;
-            const query = {email: email};
-            const result = await appointmentCollection.find(query).toArray();
-            res.send(result);
-          })
-        // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+      // carefully delete each item from the cart
+      const query = {
+        _id: {
+          $in: payment.appointmentIds.map((id) => new ObjectId(id)),
+        },
+      };
+      const deleteResult = await appointmentCollection.deleteMany(query);
+      res.send({ paymentResult, deleteResult });
+    });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('Savvy care site is running')
-})
+app.get("/", (req, res) => {
+  res.send("Savvy care site is running");
+});
 
 app.listen(port, () => {
-    console.log(`Savvy Care site is running on port ${5000}`)
-})
+  console.log(`Savvy Care site is running on port ${5000}`);
+});
